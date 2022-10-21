73a15922882df204dca3ebf60c063faae769d253
WebDeveloper7611 <gsattorov0102@mail.ru>
2022-10-21 12:10:55 +0500

    A

---
+++ assest/js/index.js
@@ -0,0 +1,9 @@
+let dropDown = document.querySelector('.dropDown')
+let dropMenu = document.querySelector('.dropMenu')
+let xMenu = document.querySelector('.x-menu')
+
+dropDown.addEventListener('click', ()=>{
+    dropMenu.classList.toggle('toggleNav') 
+    xMenu.classList.toggle('fa-bars')
+    xMenu.classList.toggle('fa-times')
+})
