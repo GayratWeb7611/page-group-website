7a6a73520b1bb97e719be38ff2fd21bfdc97c0bc
WebDeveloper7611 <gsattorov0102@mail.ru>
2022-10-24 08:02:45 +0500

    faqat btn hover effecti uxshamadi

--- assest/js/index.js
+++ assest/js/index.js
@@ -1,10 +1,35 @@
 let dropDown = document.querySelector('.dropDown')
 let dropMenu = document.querySelector('.dropMenu')
 let xMenu = document.querySelector('.x-menu')
-
+let closeMenu = document.querySelector('.drop-close')
 dropDown.addEventListener('click', ()=>{
-    dropMenu.classList.toggle('toggleNav') 
-    xMenu.classList.toggle('fa-bars')
-    xMenu.classList.toggle('fa-times')
+    dropMenu.classList.toggle('toggleNav')
+    console.log(dropMenu.classList.contains('toggleNav'));
+    if(dropMenu.classList.contains('toggleNav')){
+        xMenu.classList.remove('fa-bars')
+        xMenu.classList.add('fa-times')
+        closeMenu.style.display = 'block'
+    }else{
+        xMenu.classList.add('fa-bars')
+        xMenu.classList.remove('fa-times')
+        closeMenu.style.display = 'none'
+    }
 })
 
+closeMenu.addEventListener('click',()=>{
+    dropMenu.classList.remove('toggleNav')
+     xMenu.classList.add('fa-bars')
+        xMenu.classList.remove('fa-times') 
+    closeMenu.style.display = 'none'
+})
+
+
+let callModal = document.querySelector('.modal-calling-con')
+let callModalClose = document.querySelector('.close-calling')
+setTimeout(()=>{
+    callModal.style.display = 'grid'
+},20000)
+callModalClose.addEventListener('click',()=>{
+    callModal.style.display = 'none'
+
+})
